3bf3806230bc7fc92a064d7cb6244d08bcc8f4be
BrandonC123 <brandoc318@gmail.com>
2022-10-17 23:16:33 -0700

    Testing event emitter.

--- App.js
+++ App.js
@@ -60,3 +60,12 @@ async function promiseReadFile() {
     } catch (error) {}
 }
 promiseReadFile();
+
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on("start", () => {
+    console.log("started");
+});
+
+eventEmitter.emit("start");
